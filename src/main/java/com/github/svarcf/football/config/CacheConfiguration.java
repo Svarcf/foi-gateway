2ca4c784574b24df4783b8bc260d5947841a4640
Fran <svarcf@gmail.com>
2019-11-03 00:57:58 +0100

    Initial version of footballUI generated by JHipster-6.4.1

---
+++ src/main/java/com/github/svarcf/football/config/CacheConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.svarcf.football.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+}
